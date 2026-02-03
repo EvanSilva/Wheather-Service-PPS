a8d851943e60e8e0c5e5bc8b8b9b1a83cf5efac0
Evan Silva González <evanchapela@gmail.com>
2026-02-03 09:36:02 +0100

    Add secret usage in api

--- weatherService.js
+++ weatherService.js
@@ -1,5 +1,9 @@
+const API_KEY = process.env.OPENWEATHER_API_KEY || "demo_key";
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+
 const obtenerDatosMeteo = async (latitude, longitude) => {
   const baseUrl = "https://api.open-meteo.com/v1/forecast";
+
   const params = new URLSearchParams({
     latitude: latitude,
     longitude: longitude,
